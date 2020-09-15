33f343f57950d9ff4be813fb3609f665e68a546f
DimasBo <dimasbo.work@gmail.com>
2020-09-15 13:07:30 +0300

    lessons1 task 2

---
+++ lessons1/task2.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+Прогарма у наший метод fillArray додав модифікатор static так як без нього не можна визвати
+метод у методі Main 
+зміна len є розміром масмвиву у формулу 2 додаєм так як у нас є умова що крайні точки теж 
+повині бути вклюяні.
+
+*******************************************************************************/
+import java.util.Random;
+
+public class Main
+{   
+
+    private static int[] fillArray(int begin, int end, int step){
+        int len = (2 + end - begin) / step;
+        System.out.println("len in array");
+        System.out.println(len);
+        
+        int[] arr = new int[len];
+        
+        int val = begin;
+        
+        for(int i=0;i < len; i++ )
+        {
+            arr[i] = val;
+            val += step;
+        }
+        
+        return arr;
+    }
+    
+	public static void main(String[] args) {
+	    int[] arr = fillArray(0,10,2);
+	    
+	    System.out.println("show result");
+        for(int a : arr)
+            System.out.println(a);
+	}
+}
