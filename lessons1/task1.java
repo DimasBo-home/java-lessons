50ad2ccdd7b06a1bbd3d1359a4535c3bf34f97c5
DimasBo <dimasbo.work@gmail.com>
2020-09-15 12:51:13 +0300

    lessons1 task 1

---
+++ lessons1/task1.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+Прогарма у наший метод printArray додав модифікатор static так як без нього не можна визвати
+метод у методі Main 
+зміна len є розміром двохвимірного квадратного масмву.
+
+*******************************************************************************/
+import java.util.Random;
+
+public class Main
+{   
+	static int len = 10;
+	static int[][] arr;
+
+    private static void printArray(int[][] arr){
+        for(int[] a : arr){
+            for(int b : a)
+                System.out.print(Integer.toString(b) + " ");
+		    System.out.println();
+        }
+    }
+
+	public static void main(String[] args) {
+		arr = new int[len][len];
+		Random random = new Random();
+		
+		for(int i=0;i<len; i++)
+            for(int j=0;j<len; j++)
+                arr[i][j] = random.nextInt(10);
+        
+        printArray(arr);
+		
+		
+	}
+}
